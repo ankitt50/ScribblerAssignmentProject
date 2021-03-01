ded9ac26027f74250dfe641964442f4ff0031dde
Ankit <ankitt50@gmail.com>
2021-03-01 12:22:18 +0530

    home page UI changes

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+getheader();
+function getheader() {
+
+    document.getElementById('top-header-container').innerHTML='<div id="header-left">'+
+'<label id="main-heading">ScriBBler</label>'+
+'<label id="heading-subtext">Explore, Imagine, Create</label>'+
+'</div>'+
+'<div id="header-right">'+
+'<button class="sign-btn">Signup</button>'+
+'<button class="sign-btn">Signin</button>'+
+'</div>';
+
+}
