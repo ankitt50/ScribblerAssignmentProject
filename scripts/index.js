400e594d9274d45e27773491e3969f4e8a3b10c7
Ankit <ankitt50@gmail.com>
2021-03-05 23:12:29 +0530

    First page UI part complete

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,43 @@ function getheader() {
 '<label id="heading-subtext">Explore, Imagine, Create</label>'+
 '</div>'+
 '<div id="header-right">'+
-'<button class="sign-btn">Signup</button>'+
-'<button class="sign-btn">Signin</button>'+
+'<button class="sign-btn" data-bs-toggle="modal" data-bs-target="#signUpModal">Signup</button>'+
+'<button class="sign-btn" data-bs-toggle="modal" data-bs-target="#signInModal">Signin</button>'+
 '</div>';
 
 }
+
+getModal('signUpModal');
+getModal('signInModal');
+getModal('createPostModal');
+
+function getModal(modalName) {
+    var exampleModal = document.getElementById(modalName);
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget;
+  // Extract info from data-bs-* attributes
+  var recipient = button.getAttribute('data-bs-whatever');
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  var modalTitle = exampleModal.querySelector('.modal-title');
+  var modalBodyInput = exampleModal.querySelector('.modal-body input');
+
+  //modalTitle.textContent = 'New message to ' + recipient
+  if(modalName === 'signInModal') {
+      modalTitle.textContent = 'Welcome Back!';
+  }
+  if(modalName === 'signUpModal') {
+    modalTitle.textContent = 'Get started';
+  }
+  if(modalName === 'createPostModal') {
+      modalTitle.textContent = 'Pen Your Post';
+  }
+  
+  modalBodyInput.value = recipient;
+});
+
+}
+
