d729498f777bdba7db9246bdcaec9eb5cda2000a
Ankit <ankitt50@gmail.com>
2021-03-07 23:35:08 +0530

    Project completed except for comments in code

--- scripts/postlist.js
+++ scripts/postlist.js
@@ -1,15 +1,14 @@
 getheader();
 getModal('signUpModal');
 getModal('signInModal');
-var authors = ['Srishti Gupta','Colby Fayock','Yazeed Bzadough','Cedd Burge','Srishti Gupta','Ankit Tripathi','Saksham tripathi'];
-var title = ['\'let\' me be a \'const\'(ant), not a \'var\'(iable)!</h5>','What is linting and how can it save you time?','How to Get More Views on Your Tech Blog','How to write easily describable code','Everything you should know about \'module\' & \'require\' in Node.js','testing the functionality','testing odd blogs'];
+var authors = ['Srishti Gupta','Colby Fayock','Yazeed Bzadough','Cedd Burge','Srishti Gupta'];
+var title = ['\'let\' me be a \'const\'(ant), not a \'var\'(iable)!</h5>','What is linting and how can it save you time?','How to Get More Views on Your Tech Blog','How to write easily describable code','Everything you should know about \'module\' & \'require\' in Node.js'];
 var body = [
     'Since JavaScript does not have any type-checking, either of these keywords can be used to declare a variable of any type (datatype) in JavaScript. Though all the three keywords are used for the same purpose, they are different.',
     'One of the biggest challenges in software development is time. It\'s something we can\'t easily get more of, but linting can help us make the most out of the time we have.',
     'If you\'re a developer with a Twitter account, you\'ve already seen everyone and their cat start a blog, YouTube channel, or Patreon. We all want to become stars, or at the very least, a recognizable name in the industry.',
     'When code is not describable using words, most people have to do some mental mapping to turn it in to words. This wastes mental energy, and you run the risk of getting the mapping wrong. Different people will map to different words, which leads to confusion when discussing the code.',
-    'Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node, and you can say that you\'ve created a module named xyz.','Testing Testing  testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing',
-    'testing odd blogs,esting odd blogsesting odd blogsesting odd blogsesting odd blogsesting odd blogsesting odd blogsesting odd blogs',
+    'Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node, and you can say that you\'ve created a module named xyz.'
 ];
 getMainBody();
 function getheader() {
@@ -25,6 +24,10 @@ function getheader() {
 }
 
 function getMainBody() {
+  if(authors.length<=0){
+    document.getElementById('postlist-main-body-container').innerHTML='';
+    return;
+  }
     if(authors.length%2!=0) {
         document.getElementById('postlist-main-body-container').innerHTML+='<div class="container">';
         for(var i=0;i<authors.length-1;i+=2) {
